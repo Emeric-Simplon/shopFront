30eb3197e98f9f84d077892cfc263f7adea2e553
Emeric-cncamp <eme81380@gmail.com>
2020-04-29 11:34:40 +0200

    add product list and cart

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import * as data from '../../data/data.json';
+import * as categories from '../../data/categories.json';
+
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  public productList;
+  public categories;
+  public cart=  [];
+  public total: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.productList = data.data;
+    this.categories = categories.categories;
+  }
+  filterProduct(catType){
+    this.productList = data.data;
+    this.productList = this.productList.filter(product => product.category === catType);
+    return this.productList;
+  }
+
+  addItem(product){
+    if(this.cart.includes(product)){
+      let existingProduct = this.cart.find(prod => prod === product);
+      existingProduct.quantity += 1;
+    }else{
+      product.quantity = 1;
+      this.cart.push(product) 
+    }
+    this.refreshTotal()
+  }
+
+  reset(){
+    this.productList = data.data;
+  }
+
+  refreshTotal(){
+    let result = 0;
+    this.cart.map(item => {
+      let itemTotal = item.quantity * item.price;
+    return  result += itemTotal;
+    })
+    this.total = result;
+  }
+
+  decrementQty(cartProduct){
+    cartProduct.quantity -= 1;
+    if(cartProduct.quantity <= 0){
+     this.cart = this.cart.filter(prod => prod != cartProduct);
+    }
+    this.refreshTotal();
+  }
+
+  incrementQty(cartProduct){
+    cartProduct.quantity += 1;
+    this.refreshTotal();
+  }
+}
---
+++ src/app/models/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    id: number;
+    name: string;
+    quantity: number;
+    price: number;
+    category: string;
+    promotion: boolean;
+    description: string;
+    avalable: boolean;
+    image: string;
+  }
